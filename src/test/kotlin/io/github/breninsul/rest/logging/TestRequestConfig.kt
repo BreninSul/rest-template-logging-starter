b90c18d856bf1b33fa8af13b0dcc8f96a84b6d5b
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-02-27 20:03:01 +0100

    Add RestTemplate support to TestRequestConfig and TestService
    RestTemplate bean was introduced in TestRequestConfig alongside the existing RestClient bean. TestService was updated to include RestTemplate usage with a new `testTemplate` method for HTTP requests. These changes enhance flexibility for making web requests in tests.

--- src/test/kotlin/io/github/breninsul/rest/logging/TestRequestConfig.kt
+++ src/test/kotlin/io/github/breninsul/rest/logging/TestRequestConfig.kt
@@ -4,17 +4,20 @@ import io.github.breninsul.logging2.HttpLogSettings
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.web.client.RestClientBuilderConfigurer
+import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.http.client.BufferingClientHttpRequestFactory
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestTemplate
 import java.time.Duration
 @SpringBootConfiguration
 open class TestRequestConfig {
 
     @Bean("restClient")
-    fun googleMapsRestClient(configurer: ObjectProvider<RestClientBuilderConfigurer>): RestClient {
+    fun restClient(configurer: ObjectProvider<RestClientBuilderConfigurer>): RestClient {
         var connectTimeout: Duration = Duration.ofSeconds(10)
         var readTimeout: Duration = Duration.ofSeconds(120)
         val properties = RestTemplateLoggerProperties(response = HttpLogSettings(bodyIncluded = false))
@@ -32,7 +35,25 @@ open class TestRequestConfig {
         val client = builder.build()
         return client
     }
+    @Bean("restTemplate")
+    fun restTemplate(configurer: ObjectProvider<RestTemplateBuilderConfigurer>): RestTemplate {
+        var connectTimeout: Duration = Duration.ofSeconds(10)
+        var readTimeout: Duration = Duration.ofSeconds(120)
+        val properties = RestTemplateLoggerProperties(response = HttpLogSettings(bodyIncluded = false))
+        val simpleClientHttpRequestFactory = HttpComponentsClientHttpRequestFactory()
+        simpleClientHttpRequestFactory.setConnectTimeout(connectTimeout.toMillis().toInt())
+        simpleClientHttpRequestFactory.setConnectionRequestTimeout(readTimeout.toMillis().toInt())
+        val factory = BufferingClientHttpRequestFactory(simpleClientHttpRequestFactory)
+        val builder = RestTemplateBuilder()
 
+        builder.requestFactory{->factory}
+        builder.interceptors(RestTemplateLoggerConfiguration().registerRestTemplateLoggingInterceptor(properties))
+
+        val configurers = configurer.toList()
+        configurers.forEach { it.configure(builder) }
+        val client = builder.build()
+        return client
+    }
     @Bean
     fun testService(restClient: RestClient): TestService {
         return TestService(restClient)
--- src/test/kotlin/io/github/breninsul/rest/logging/TestService.kt
+++ src/test/kotlin/io/github/breninsul/rest/logging/TestService.kt
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.exchange
 import java.io.InputStream
 open class TestService(
-    val restClient: RestClient
+    val restClient: RestClient,
+    val restTemplate: RestTemplate
+
 ) {
 
 
@@ -17,6 +21,14 @@ open class TestService(
             }
         return response
     }
-
+    open fun testTemplate():InputStream {
+        val response =
+            restTemplate
+                .
+                .get().uri("https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp").exchange { _, rs ->
+                rs.body
+            }
+        return response
+    }
 
 }
